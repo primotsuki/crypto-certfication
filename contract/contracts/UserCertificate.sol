cf97df1a9177bc054adb38b151ad07ae1cb8d43e
primoac <primotsuki@gmail.com>
2019-11-20 23:47:21 -0400

    datos cetificado

--- contract/contracts/UserCertificate.sol
+++ contract/contracts/UserCertificate.sol
@@ -28,7 +28,7 @@ contract UserCertificate {
   }
 
   event CertificateIssued(address indexed _from, address indexed _to, string _ipfsHash);
-  event CertificateRevoked(address indexed _from, adrress indexed _to, string _ipfsHash);
+  event CertificateRevoked(address indexed _from, address indexed _to, string _ipfsHash);
 
   constructor() public {
     owner = msg.sender;
@@ -38,4 +38,62 @@ contract UserCertificate {
     price = _price;
   }
 
+  function withdrawFounds(uint amount) public payable onlyOwner returns(bool) {
+    require(amount <= address(this).balance, 'amount is bigger than balance');
+    owner.transfer(amount);
+    return true;
+  }
+
+  function certificateExists(string memory ipfsHash) public view returns(bool) {
+    if(certificates[ipfsHash]==true) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  function addCertificate(address user, string  memory ipfsHash) public payable costs(price) {
+    require (msg.sender != user, 'cannot certificate your self');
+    require(!certificateExists(ipfsHash), 'certificate already exists');
+
+    userCertificates[msg.sender].push(Certificate(user,ipfsHash));
+    certificates[ipfsHash] = true;
+
+    Certificate(msg.sender, user, ipfsHash);
+  }
+  function revokeCertificate(address user, string memory ipfsHash) public {
+    require(certificatesExists(ipfsHash), 'certificate does not exist');
+    Certificate[] storage certs = userCertificates[user];
+    bool found = false;
+    uint i = 0;
+    while (!found && i<certs.length) {
+			found = (stringsEqual(certs[i].ipfsHash, ipfsHash));
+			i++;
+		}
+
+		require(found, 'no found');//exists
+		require(certs[i-1].issuer == msg.sender || msg.sender == owner, ' certificate not found');//being revoked for same issuer or owner
+
+		delete certs[i-1];
+		if (certs.length > 1) {
+			certs[i-1] = certs[certs.length-1];//fill the gap
+		}
+
+		certs.length--;
+		certificates[ipfsHash] = false;
+
+		CertificateRevoked(msg.sender, user, ipfsHash);
+  }
+  //Return the number of certificates associated to user
+	function getNumberCertificatesUser(address user) public constant returns (uint) {
+		return userCertificates[user].length;
+	}
+
+	//Returns certificate for user in specific index position. This is neccesary because currently it is not possible to return array of strings
+	function getUserCertificateAtIndex(address user, uint256 index) public constant returns (address, string) {
+		require(userCertificates[user].length>index);
+
+		Certificate storage cert = userCertificates[user][index];
+		return (cert.issuer, cert.ipfsHash);
+	}
+}  
 }
--- ngapp/src/app/pages/certificado-emision/certificado-emision.component.ts
+++ ngapp/src/app/pages/certificado-emision/certificado-emision.component.ts
@@ -28,16 +28,16 @@ export class CertificadoEmisionComponent implements OnInit {
       console.log(params);
       this.solicitud_id = params.id;
       this.CertForm = this.formBuilder.group({
-        titulo_curso: ['', Validators.required],//
-        fecha_emision: ['', Validators.required],//
+        titulo_curso: ['', Validators.required],
+        fecha_emision: ['', Validators.required],
         alumno_id: [params.user_id],
         alumno_nombre: ['', Validators.required],
         alumno_dni: ['', Validators.required],
-        institucion_id: ['', Validators.required], //
-        institucion_nombre: ['', Validators.required],//
-        tipo_solicitud_id: [1],//
+        institucion_id: ['', Validators.required],
+        institucion_nombre: ['', Validators.required],
+        tipo_solicitud_id: [1],
         wallet: ['', Validators.required],
-        solicitud_id: [params.solicitud_id] 
+        solicitud_id: [params.solicitud_id]
       });
       this.get_datos_persona();
     });
@@ -46,7 +46,29 @@ export class CertificadoEmisionComponent implements OnInit {
   get_datos_persona() {
     this.pagesService.getDatosUsuario(this.f.alumno_id.value).subscribe(data=>{
       console.log(data);
+      this.f.institucion_nombre.setValue(data.data.instituciones[0].nombre);
+      this.f.institucion_id.setValue(data.data.instituciones[0].institiducion_id);
+      this.f.alumno_nombre.setValue(`${data.data.nombre} ${data.data.apellido_paterno} ${data.data.apellido_materno}`)
     });
+  }
+  Osubmit() {
+    this.submitted = true;
+    if (this.CertForm.invalid) {
+      return;
+    } else {
+      const certificado = {
+        titulo_curso: this.f.titulo_curso.value,
+        fecha_emision: this.f.fecha_emision.value,
+        alumno_id: this.f.alumno_id.value,
+        alumno_nombre: this.f.alumno_nombre.value,
+        alumno_dni: this.f.alumno_dni.value,
+        institucion_id: this.f.institucion_id.value,
+        institucion_nombre: this.f.institucion_nombre.value,
+        tipo_solicitud_id: this.f.tipo_solicitud_id.value,
+        wallet: this.f.wallet.value,
+        solicitud_id: this.f.solicitud_id.value
+      }
+    }
   }
 
 }
