5f2097db8e401cd7485c8cded649044d763fd1c2
primotsuki <primotsuki@gmail.com>
2019-11-26 11:59:12 -0400

    logout

--- ngapp/src/app/pages/pages.component.ts
+++ ngapp/src/app/pages/pages.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MENU_ITEMS } from './pages-menu';
+import {AuthService} from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'ngx-pages',
@@ -7,5 +9,15 @@ import { MENU_ITEMS } from './pages-menu';
     templateUrl: './pages.component.html',
 })
 export class PagesComponent {
+
+    constructor(private authService: AuthService,
+                private router: Router) {
+
+    }
     menu = MENU_ITEMS;
+
+    logout(){
+        this.authService.logout();
+        this.router.navigate(['/auth/login']);
+    }
 }
